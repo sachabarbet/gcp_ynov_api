d050408e77af3554baf0bafa690e659ec3300338
sachabarbet <sacha.barbet@proton.me>
2025-05-19 14:56:40 +0200

    Add hello world controller

---
+++ src/main/java/io/github/sachabarbet/gcp_ynov_api/controllers/HelloWorldController.java
@@ -0,0 +1,20 @@
+package io.github.sachabarbet.gcp_ynov_api.controllers;
+
+import jakarta.persistence.Entity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+}
